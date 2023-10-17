2d4c4445895fc4db414220d1fa5d62f8a49c7122
Poorav <118240170+poorav03@users.noreply.github.com>
2023-10-17 20:24:30 +0530

    Add files via upload

---
+++ Lecture12-I.cpp
@@ -0,0 +1,12 @@
+// Lecture 12 I - Introduction to Pointers Pt 1
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    float b;
+
+    cout << "The address of a is "<< &a << endl;
+    cout << "The address of b is "<< &b << endl;
+    return 0;
+}
